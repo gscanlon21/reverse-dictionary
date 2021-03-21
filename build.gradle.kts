3a77e5a2d0a5b48bddcedc8206374d7069cec0ae
Graham Scanlon <gscanlon21@gmail.com>
2021-03-21 14:39:58 -0500

    build: update com.google.gms:google-services to 4.3.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:4.0.1")
         classpath(kotlin("gradle-plugin", version = "1.3.72")) // Kotlin Gradle plugin
-        classpath("com.google.gms:google-services:4.3.3") // Google Services Gradle plugin
+        classpath("com.google.gms:google-services:4.3.5") // Google Services Gradle plugin
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.5.1") // Crashlytics Gradle plugin
         classpath("org.jmailen.gradle:kotlinter-gradle:2.3.2") // ktlint
         // NOTE: Do not place your application dependencies here; they belong
