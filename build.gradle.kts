b0a21e40e2b9f96cd43a4929f0bb2c88987415b6
Graham Scanlon <gscanlon21@gmail.com>
2021-03-21 14:39:38 -0500

    build: update com.google.firebase:firebase-crashlytics-gradle to 2.5.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
         classpath("com.android.tools.build:gradle:4.0.1")
         classpath(kotlin("gradle-plugin", version = "1.3.72")) // Kotlin Gradle plugin
         classpath("com.google.gms:google-services:4.3.3") // Google Services Gradle plugin
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.2.0") // Crashlytics Gradle plugin
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.5.1") // Crashlytics Gradle plugin
         classpath("org.jmailen.gradle:kotlinter-gradle:2.3.2") // ktlint
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
