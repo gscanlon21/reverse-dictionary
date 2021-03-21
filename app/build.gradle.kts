998aa9b6d3ea94ddb078a06dd5c45dca7695b475
Graham Scanlon <gscanlon21@gmail.com>
2021-03-21 14:33:11 -0500

    build: update androidx.test.ext:junit-ktx to 1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -145,7 +145,7 @@ dependencies {
     testImplementation("androidx.test:runner:1.2.0") // AndroidJUnitRunner and JUnit Rules
     testImplementation("androidx.test:rules:1.2.0") // AndroidJUnitRunner and JUnit Rules
     testImplementation("androidx.test.ext:junit:1.1.1") // AndroidJUnitRunner and JUnit Rules
-    testImplementation("androidx.test.ext:junit-ktx:1.1.1") // AndroidJUnitRunner and JUnit Rules
+    testImplementation("androidx.test.ext:junit-ktx:1.1.2") // AndroidJUnitRunner and JUnit Rules
     testImplementation("androidx.test.espresso:espresso-core:3.3.0")
     testImplementation("androidx.arch.core:core-testing:2.1.0")
     testImplementation("org.robolectric:robolectric:4.5.1")
