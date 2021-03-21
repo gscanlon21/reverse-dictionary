2c2803d297138202d4b7fa1c46625ad744aabf1d
Graham Scanlon <gscanlon21@gmail.com>
2021-03-21 14:20:10 -0500

    build: update androidx.core:core-ktx to 1.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,7 +100,7 @@ dependencies {
     implementation("com.google.android.material:material:1.3.0")
 
     // Kotlin Extensions
-    implementation("androidx.core:core-ktx:1.3.1")
+    implementation("androidx.core:core-ktx:1.3.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.2.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.3.0")
