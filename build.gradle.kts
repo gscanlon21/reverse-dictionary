e65830a25f022de0a7d7e41fc57a69b8d64f57f9
Graham Scanlon <gscanlon21@gmail.com>
2023-02-21 20:40:16 -0600

    chore: update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
         maven("https://plugins.gradle.org/m2/")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.0.1")
+        classpath("com.android.tools.build:gradle:4.2.2")
         classpath(kotlin("gradle-plugin", version = Dependencies.KOTLIN_VERSION)) // Kotlin Gradle plugin
         classpath("org.jmailen.gradle:kotlinter-gradle:3.3.0") // ktlint
         // NOTE: Do not place your application dependencies here; they belong
